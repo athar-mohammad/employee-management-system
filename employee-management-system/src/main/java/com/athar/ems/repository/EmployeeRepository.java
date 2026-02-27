1a880d8080592240de9ad5289d06f10482531052
athar-mohammad <athar.m@turing.com>
2026-02-28 02:27:43 +0530

    EMS-1: Fixed repository generics and corrected service annotations

--- employee-management-system/src/main/java/com/athar/ems/repository/EmployeeRepository.java
+++ employee-management-system/src/main/java/com/athar/ems/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.athar.ems.repository;
 
 import com.athar.ems.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface EmployeeRepository extends JpaRepository<Long, Employee> {
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
